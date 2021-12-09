bff379631c381d3b0520b74aff405b5b57e74cef
karomol <63105440+karomol@users.noreply.github.com>
2021-12-09 07:23:07 +0200

    Update test.py

--- lab_5/test.py
+++ lab_5/test.py
@@ -1,21 +1,20 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
-
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 def test():
     search_word = 'nokia'
 
-    driver = webdriver.Chrome()
     driver.implicitly_wait(15)
 
     driver.get('https://allo.ua/')
 
-    driver.find_element_by_id("search-form__input").send_keys(search_word)
-    driver.find_element_by_id("search-form__input").send_keys(Keys.ENTER)
-
+    driver.find_element(By.ID, "search-form__input").send_keys(search_word, Keys.ENTER)
     time.sleep(3)
-    result = driver.find_element_by_css_selector('div.product-card__content').text
+    result = driver.find_element(By.CSS_SELECTOR, 'div.product-card__content').text
 
     assert result.lower().__contains__(search_word.lower()), 'Error, the search word does not match the result'
 
